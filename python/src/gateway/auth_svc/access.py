c42420a167a88c773cdb7217de8081902fe09de7
Reuben Abraham <reuben.abraham20@gmail.com>
2023-02-08 14:35:42 -0500

    Fixing typos in gateway service

--- python/src/gateway/auth_svc/access.py
+++ python/src/gateway/auth_svc/access.py
@@ -1,18 +1,28 @@
 import os, requests
+import logging
+
+log = logging.getLogger(f"{__name__}.sub")
 
 def login(request):
+    log.warning("We're in login")
+    log.warning(f"Request: {request}")
     auth = request.authorization
+    log.warning(f"Auth:{auth}")
     if not auth:
+        log.warning("In not auth")
         return None, ("missing credentials", 401)
 
     basicAuth = (auth.username, auth.password)
+    log.warning(f"{basicAuth}: {auth.username} , {auth.password}")
 
     response = requests.post(
-        f"http://{os.environ.get('AUTH_SVC_ADDRESS')}/login",
-        auth=basicAuth
+        f"http://{os.environ.get('AUTH_SVC_ADDRESS')}/login", auth=basicAuth
     )
+    log.warning(f"Response code: {response}")
 
     if response.status_code == 200:
-        return response.txt, None
+        log.warning("Successful response")
+        return response.text, None
     else:
-        return None, (response.txt, response.status_code)
+        log.warning("Unsuccessful response")
+        return None, (response.text, response.status_code)
